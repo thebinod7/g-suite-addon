bd660acd168488b230f9872284bf94eb8e664c17
Binod <thebinod7@gmail.com>
2023-07-04 14:59:58 +0545

    submit form with static input fields

--- Code.js
+++ Code.js
@@ -1,45 +1,38 @@
-function onDefaultHomePageOpen() {
-  console.log("Home!!!");
+function onDefaultHomePageOpen(){
+  console.log("Home!!!")
 }
 
-function createFormInputByType(input) {
-  switch (input.type) {
-    case "text": {
-      var textInput = CardService.newTextInput()
+function createFormInputByType(input){
+  switch(input.type){
+    case 'text': {
+       var textInput = CardService.newTextInput()
         .setFieldName("myTextBox")
         .setTitle("Enter your text")
         .setHint("Type here");
 
-      return textInput;
+        return textInput;
     }
-    case "dropdown": {
-      var dropdown = CardService.newSelectionInput()
-        .setType(CardService.SelectionInputType.DROPDOWN)
-        .setTitle("A group of radio buttons.")
-        .setFieldName("checkbox_field")
-        .addItem("Option 1", "option_1", true)
-        .addItem("Option 2", "option_2", false)
-        .addItem("Option 3", "option_3", false);
+    case 'dropdown': {
+           var dropdown = CardService.newSelectionInput()
+            .setType(CardService.SelectionInputType.DROPDOWN)
+            .setTitle("A group of radio buttons.")
+            .setFieldName("checkbox_field")
+            .addItem("Option 1", "option_1", true)
+            .addItem("Option 2", "option_2", false)
+            .addItem("Option 3", "option_3", false);
 
-      return dropdown;
+            return dropdown;
     }
 
-    default:
-      return "";
+    default: return ''
   }
-}
-
-function createCardWithTitle(title) {
-  return CardService.newCardBuilder().setHeader(
-    CardService.newCardHeader().setTitle(title)
-  );
 }
 
 // Create Card Section
 // Loop through sample data
 // Get input by type
 // Add as widget in chain
-function renderSettingsForm() {
+function renderSettingsForm(){
   // Fetch account ID
   // Fetch settings by account ID
   // Display fields based on settings
@@ -47,72 +40,61 @@ function renderSettingsForm() {
   // Submit field values to Monday board
 }
 var sample_data = [
-  { id: "name", title: "Item Name", type: "text", value: "" },
-  { id: "dropdown", title: "Dropdown", type: "dropdown", value: "" },
-  { id: "email", title: "Email Address", type: "text", value: "" },
-];
-
-function onGmailMessageOpen(e) {
-  renderSettingsForm();
-  // Activate temporary Gmail scopes, in this case to allow
-  // message metadata to be read.
-  var accessToken = e.gmail.accessToken;
-  GmailApp.setCurrentMessageAccessToken(accessToken);
-
-  var messageId = e.gmail.messageId;
-  var message = GmailApp.getMessageById(messageId);
-  var subject = message.getSubject();
-  var sender = message.getFrom();
-
-  var textInput = CardService.newTextInput()
-    .setFieldName("myTextBox")
-    .setTitle("Enter your text");
-
-  var dropdown = CardService.newSelectionInput()
-    .setType(CardService.SelectionInputType.DROPDOWN)
-    .setTitle("-Select Options-")
-    .setFieldName("dropdown_field")
-    .addItem("Option 1", "option_1", true)
-    .addItem("Option 2", "option_2", false)
-    .addItem("Option 3", "option_3", false);
-
-  var _card = createCardWithTitle("Contact Details");
-
-  var displayCard = _card
-    .addSection(
-      CardService.newCardSection()
-        .addWidget(
-          CardService.newKeyValue().setTopLabel("Subject").setContent(subject)
-        )
-        .addWidget(
-          CardService.newKeyValue().setTopLabel("From").setContent(sender)
-        )
-    )
-    .build();
-
-  var dynamicCard;
-
-  if (sample_data.length) {
-    dynamicCard = createCardWithTitle();
-    for (var i = 0; i < sample_data; i++) {
-      console.log(sample_data[i]);
-      var _input = createFormInputByType(sample_data[i]);
-      dynamicCard
-        .addSection(CardService.newCardSection().addWidget(_input))
-        .build();
-    }
-
-    return [displayCard, dynamicCard];
-  } else {
-    var displayCard = _card
-      .addSection(CardService.newCardSection())
-      .addWidget(
-        CardService.newKeyValue().setTopLabel("Subject").setContent(subject)
-      )
-      .addWidget(
-        CardService.newKeyValue().setTopLabel("From").setContent(sender)
-      )
-      .build();
-    return [displayCard];
+  {id:'name', title:"Item Name", type:'text', value:''},
+  {id:'dropdown', title:'Dropdown', type:'dropdown', value:''},
+  {id:'email', title:'Email Address', type:'text', value:''},
+]
+  function createCard(title){
+    return CardService.newCardBuilder()
+        .setHeader(CardService.newCardHeader()
+            .setTitle(title))
   }
+
+  function onGmailMessageOpen(e) {
+     var card = CardService.newCardBuilder();
+  
+     var formAction = CardService.newAction()
+        .setFunctionName('handleFormSubmit');
+    
+      var inputField = CardService.newTextInput()
+        .setFieldName('name')
+        .setTitle('Name')
+
+      var emailField = CardService.newTextInput()
+        .setFieldName('email')
+        .setTitle('Email')
+    
+      var submitButton = CardService.newTextButton()
+        .setText('Submit')
+        .setOnClickAction(formAction);
+    
+      var section = CardService.newCardSection()
+        .addWidget(inputField)
+        .addWidget(emailField)
+        .addWidget(submitButton);
+    
+      card.addSection(section);
+  return card.build();
+
+    // var html = HtmlService.createTemplateFromFile("sidebar").evaluate().getContent();
+    // return CardService.newCardBuilder()
+    // .setHeader(CardService.newCardHeader().setTitle('Contact Details'))
+    // .addSection(CardService.newCardSection().addWidget(CardService.newKeyValue().setContent(html)))
+    // .build();
+}
+
+function handleFormSubmit(e) {
+  var name = e.formInput['name'];
+  var email = e.formInput['email'];
+
+  console.log(name,email);
+  
+  var message = CardService.newTextParagraph()
+    .setText('Form submitted successfully!');
+    
+  var updatedCard = CardService.newCardBuilder()
+    .addSection(CardService.newCardSection().addWidget(message))
+    .build();
+    
+  return updatedCard;
 }
